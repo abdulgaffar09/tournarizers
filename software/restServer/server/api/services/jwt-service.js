2d2725191280749843286ad2a90fd45a62e19943
sreeharsha <harsha.konduru@phenompeople.com>
2020-04-11 16:15:57 +0530

    1.Jwt token service changes for validating and getting profile

--- software/restServer/server/api/services/jwt-service.js
+++ software/restServer/server/api/services/jwt-service.js
@@ -1,26 +1,85 @@
 import jwt from 'jsonwebtoken';
-class JwtService{
+import MongoManager from '../db/mongo-manager';
+import UserService from '../services/user-service';
+class JwtService {
 
-    static createJwtToken(profile){
-        return jwt.sign({emailId:profile.emailId, userId: profile.userId},"tournaXYZ",{expiresIn:'4h'});
+    static createJwtToken(profile) {
+        return jwt.sign({ emailId: profile.emailId, userId: profile.userId }, "tournaXYZ", { expiresIn: '24h' });
     }
 
-    static validateJwtToken(jwtToken,profile){
-        return new Promise((resolve,reject) => {
+    static validateJwtToken(jwtToken, profile) {
+        return new Promise((resolve, reject) => {
             let jwtRes = {}
-            jwt.verify(jwtToken,"tournaXYZ",(err,decoded) => {
-                if(decoded.emailId == profile.emailId && decoded.userId == profile.userId){
+            jwt.verify(jwtToken, "tournaXYZ", (err, decoded) => {
+                if (err) {
                     jwtRes = {
-                        status : 'success',
+                        status: 'failure',
+                        message: "Invalid jwtToken"
+                    }
+                    reject(jwtRes);
+                }
+                if (!profile || !Object.keys(profile).length && decoded.userId) {
+                    const mongoClient = MongoManager.getMongoClient();
+                    UserService.getUser(mongoClient, decoded.userId).then(profile => {
+                        profile = profile;
+                        jwtRes = {
+                            status: 'success',
+                            jwtToken: this.createJwtToken(profile)
+                        }
+                        resolve(jwtRes);
+                    }).catch(err => {
+                        jwtRes = {
+                            status: 'failure',
+                            error: err
+                        }
+                        resolve(err)
+                    })
+                } else if (decoded.emailId == profile.emailId && decoded.userId == profile.userId) {
+                    jwtRes = {
+                        status: 'success',
                         jwtToken: this.createJwtToken(profile)
                     }
                     resolve(jwtRes);
-                }else{
-                    reject(new Error("Invalid jwtToken."));
+                } else {
+                    jwtRes = {
+                        status: 'failure',
+                        message: "Invalid jwtToken"
+                    }
+                    reject(jwtRes);
                 }
             });
-        });
-        
+        })
+    }
+
+    static validateJwtTokenAndGetProfile(jwtToken) {
+        return new Promise((resolve, reject) => {
+            let jwtRes = {}
+            jwt.verify(jwtToken, "tournaXYZ", (err, decoded) => {
+                if (err) {
+                    jwtRes = {
+                        status: 'failure',
+                        message: "Invalid jwtToken"
+                    }
+                    reject(jwtRes);
+                }
+                const mongoClient = MongoManager.getMongoClient();
+                UserService.getUser(mongoClient, decoded.userId).then(profile => {
+                    profile = profile;
+                    jwtRes = {
+                        status: 'success',
+                        jwtToken: this.createJwtToken(profile),
+                        profile: profile
+                    }
+                    resolve(jwtRes);
+                }).catch(err => {
+                    jwtRes = {
+                        status: 'failure',
+                        error: err
+                    }
+                    resolve(err)
+                })
+            });
+        })
     }
 }
 
